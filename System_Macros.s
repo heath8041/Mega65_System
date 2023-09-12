4824029c5ed641fb0ecfeef31cdc2efe15eb9e16
heath8041 <heath.gallimore@gmail.com>
2023-09-12 16:44:55 -0500

    adding color ram changes to build

--- System_Macros.s
+++ System_Macros.s
@@ -1,3 +1,5 @@
+#import "../Mega65_System/memorymap.asm"
+
 /**
  * .macro BasicUpstart65
  * 
@@ -38,3 +40,32 @@
 	end:
 		.byte $00,$00	//End of basic terminators
 }
+
+/*
+This function uses the VIC III banking register bit 1
+to enable 2K of color ram at $D800 in bank 0.
+the default configuration only has 1K color memory in bank 0
+you need to turn this on if you are doing 80 char colums
+otherwise you'll only get half of the screen to change colors.
+*/
+.macro System_Enable2KColorRamInBank0(){
+	lda VIC.BANKING_REG //load it to acc
+	ora #%00000001 //or with 1 to set only bit 1
+	sta VIC.BANKING_REG //store it back
+}
+
+// .macro System_Store_Color_Ram_Pointer_on_Base_Page(){
+//-------untested ---------
+// //Store address $1F800 at base page address $40 - $43 .
+// // ( Rememb er to use Little Endian order .)
+// 	lda # $00
+// 	sta $40
+// 	lda # $F8
+// 	sta $41
+// 	lda # $01
+// 	sta $42
+// 	lda # $00 	
+// 	sta $43
+// }
+
+
--- memorymap.asm
+++ memorymap.asm
@@ -3,12 +3,13 @@ VIC:{
 
     .label SCREEN = $0800
     .label MEMORY_SETUP = $D018
-    .label COLOR_RAM = $D800
+    .label COLOR_RAM = $D800 //this is only 1K until you set $D030 bit 1
     .label BORDER_COLOR = $D020
     .label SCREEN_COLOR = $D021
     .label RASTER_LINE = $D012
     .label YSCROLL = $D017
     .label XSCROLL = $D016
+    .label BANKING_REG = $D030
 }
 
 SID:{
